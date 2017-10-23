ac6ecdbd105f3bfb8c55f0c6c7a1d8ba38f3ae99
dododb <dorian.bataille@viacesi.fr>
2017-10-23 17:07:12 +0200

    mise en place de la stratégie d'envoie des trame

---
+++ src/Ordre/Order.java
@@ -0,0 +1,27 @@
+package Ordre;
+
+public abstract class Order {
+	public static int ORDRE_TYPE_CONSIGNE = 0;
+	public static int ORDRE_TYPE_VEILLE = 1;
+	
+	private static int tramesIndex[] = { 15, 0 };
+	
+	public static int OrdreToSend(int trameValue[], int OrdreType)
+	{
+		int trameToSend = OrdreType<<getDecalage(0);
+		for(int i = 1; i<tramesIndex.length; i++)
+		{
+			trameToSend += trameValue[i-1]<<getDecalage(i);
+		}
+		return trameToSend;
+	}
+	private static int getDecalage(int index)
+	{
+		int decal = 0;
+		for(int i = index; i<tramesIndex.length;i++)
+		{
+			decal += tramesIndex[i];
+		}
+		return decal;
+	}
+}
