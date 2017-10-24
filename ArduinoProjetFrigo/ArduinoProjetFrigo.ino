3dfb23ca5b253a3f48e44945c35eb4ccf042d7fe
dododb <dorian.bataille@viacesi.fr>
2017-10-24 11:15:42 +0200

    arduino PID

--- ArduinoProjetFrigo/ArduinoProjetFrigo.ino
+++ ArduinoProjetFrigo/ArduinoProjetFrigo.ino
@@ -1,52 +1,65 @@
+#include "PID_v1.h"
+
 int pin = 13;
 int ledPin = 6;
 int a = 0;
 
-int NUMBER_OF_TRAME = 2;
-int TRAME_INDEX[] = { 15, 0 };
+const int NUMBER_OF_TRAME = 2;
+const int TRAME_INDEX[] = { 6, 0 };
+int receiveTrames[2];
+
+double Setpoint, Input, Output;
+PID myPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
 
 void setup() {
-Serial.begin(9600);
-pinMode(pin,INPUT);
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+  Setpoint = 18;
+  myPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
-  
-  int data = digitalRead(pin);
-  int order;
-  
-  if(Serial.available()){
-    order = Serial.read(); 
-    a = order;
-  }
 
-  analogWrite(ledPin, a);
+  
+  getOrder(receiveTrames);
+
+  delay(1000);
+
+  Send(receiveTrames[0], receiveTrames[1], a);
+
   delay(1000);
 }
 
-void Send(int data1){  
-  Serial.println(data1);
+void Send(int data1, int data2, int data3) {
+  Serial.print(data1);
+  Serial.print(":");
+  Serial.print(data2);
+  Serial.print(":");
+  Serial.println(data3);
 }
 
-int[] OrdreToSend(int trame)
+bool getOrder(int* trames)
 {
-  int trames[2];
-  int ordreType = trame>>TRAME_INDEX[0];
-  trame -= ordreType;
+   if (Serial.available()) {
+    int trame = digitalRead(pin);
+    int ordreType = trame >> TRAME_INDEX[0];
+    trame -= ordreType << TRAME_INDEX[0];
+  
+    int ordre = trame >> TRAME_INDEX[2];
+  
+    trames[0] = ordreType;
+    trames[1] = ordre;
+    return true;
+   }
+   return false;
+}
 
-  int ordre = trame>>TRAME_INDEX[2];
-  
-  trames[0] = ordreType;
-  trame[1] = ordre;
-  return trames;
-}
-  
-int getDecalage(int index)
+void funcPID()
 {
-  int decal = 0;
-  for(int i = index; i<tramesIndex.length;i++)
-  {
-    decal += tramesIndex[i];
-  }
-  return decal;
+  Input = analogRead(1);
+  Input = (5*Input*100/1024);
+  myPID.Compute();
+  analogWrite(3,Output);
+  Serial.print(Input); Serial.println(Output);
 }
+
